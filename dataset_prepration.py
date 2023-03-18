1cc0e9b84f52623967bcb4f7176f9536b23476f9
farazML <faraz.fatahnaie@gmail.com>
2023-03-18 13:24:04 +0330

    fix dataset_prepration.py

--- dataset_prepration.py
+++ dataset_prepration.py
@@ -4,7 +4,7 @@ from torch.utils.data import Dataset
 import torch.nn as nn
 from PIL import Image
 from pathlib import Path
-
+import numpy as np
 
 data_dir = ".\data\Images"
 class_dict = {}
@@ -19,10 +19,9 @@ for i, d in enumerate(os.listdir(data_dir)):
         class_dict[d] = i
 
 # Create a list of image paths and labels
-data = []
+class_path = []
 for class_name in class_dict.keys():
     class_path = Path(os.path.join(data_dir, class_name))
-
     class_images = []
     for image_file in os.listdir(class_path):
         if image_file.endswith(".jpg"):
@@ -32,45 +31,27 @@ for class_name in class_dict.keys():
     random.shuffle(class_images)
     n_train = int(len(class_images) * 0.6)
     n_val = int(len(class_images) * 0.2)
-    train_images.append(class_images[0])
-    train_images.append(class_images[1:n_train+1])
-    val_images.append(class_images[n_train+1:n_train + n_val+1])
-    test_images.append(class_images[n_train + n_val + 1:])
-    # Add at least one image per class to the train set
+    # train_images.extend(class_images[0])
+    train_images.extend(class_images[0: n_train])
+    val_images.extend(class_images[n_train: n_train + n_val])
+    test_images.extend(class_images[n_train + n_val:])
 
-    # Add the images to the main data list
 
 # Write the data to text files
 with open("data/train.txt", "w") as f:
     for c in train_images:
-        if len(c) == 2:
-            image_path, label = c
-            f.write(f"{image_path} {label}\n")
-        else:
-            for i in c:
-                image_path, label = i
-            f.write(f"{image_path} {label}\n")
+        image_path, label = c
+        f.write(f"{image_path} {label}\n")
 
 with open("data/val.txt", "w") as f:
     for c in train_images:
-        if len(c) == 2:
-            image_path, label = c
-            f.write(f"{image_path} {label}\n")
-        else:
-            for i in c:
-                image_path, label = i
-            f.write(f"{image_path} {label}\n")
-
+        image_path, label = c
+        f.write(f"{image_path} {label}\n")
 
 with open("data/test.txt", "w") as f:
     for c in train_images:
-        if len(c) == 2:
-            image_path, label = c
-            f.write(f"{image_path} {label}\n")
-        else:
-            for i in c:
-                image_path, label = i
-            f.write(f"{image_path} {label}\n")
+        image_path, label = c
+        f.write(f"{image_path} {label}\n")
 
 
 # Define the custom dataset class
@@ -80,6 +61,7 @@ class MITIndoorDataset(Dataset):
         self.transform = transform
         with open(self.data_file, "r") as f:
             self.data = f.readlines()
+            print(len(self.data))
 
     def __len__(self):
         return len(self.data)
@@ -91,7 +73,3 @@ class MITIndoorDataset(Dataset):
         if self.transform:
             image = self.transform(image)
         return image, int(label)
-
-
-
-
